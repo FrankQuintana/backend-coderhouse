104b68da8f3a9c37ac80bc1bd4e27d34c0fa4f42
Frank Quintana <ferickquintana@gmail.com>
2023-11-15 17:19:20 -0300

    entrega/desafio 4

--- entrega4/src/routes/views.js
+++ entrega4/src/routes/views.js
@@ -5,6 +5,17 @@ const router = Router();
 
 const productManager = new ProductManager;
 
+router.get('/', async (req, res) => {
+    try {
+        const productos = await productManager.getProducts();
+        let datos = { productos }
+
+        res.render('home', datos)
+    } catch (error) {
+        console.log(error)
+    }
+});
+
 router.get('/products', async (req, res) => {
     try {
         const productos = await productManager.getProducts();
